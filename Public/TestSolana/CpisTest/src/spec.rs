3cdffba968beabab7e55e7289f6808ef0fbe891f
Taz <46790424+ozcodes@users.noreply.github.com>
2025-07-20 16:28:46 +0300

    Merge branch 'master' into oz/fix-nightly
    2eeb66aa79b563057df87cf5d7bb7bc98ca4bb48

--- Public/TestSolana/CpisTest/src/spec.rs
+++ Public/TestSolana/CpisTest/src/spec.rs
@@ -14,7 +14,26 @@ use solana_program::account_info::{next_account_info, AccountInfo};
 cvlr_solana::cvlr_solana_init!();
 
 #[rule]
-pub fn rule_transfer_token_transfers_same_wallet() {
+pub fn rule_transfer_token_transfers_same_wallet_0() {
+    rule_transfer_token_transfers_same_wallet::<0>();
+}
+
+#[rule]
+pub fn rule_transfer_token_transfers_same_wallet_1() {
+    rule_transfer_token_transfers_same_wallet::<1>();
+}
+
+#[rule]
+pub fn rule_transfer_token_transfers_same_wallet_2() {
+    rule_transfer_token_transfers_same_wallet::<2>();
+}
+
+#[rule]
+pub fn rule_transfer_token_transfers_same_wallet_3() {
+    rule_transfer_token_transfers_same_wallet::<3>();
+}
+
+pub fn rule_transfer_token_transfers_same_wallet<const N_SIGNERS: usize>() {
     let account_infos = cvlr_deserialize_nondet_accounts();
     let account_info_iter = &mut account_infos.iter();
     let token_program: &AccountInfo = next_account_info(account_info_iter).unwrap();
@@ -33,7 +52,7 @@ pub fn rule_transfer_token_transfers_same_wallet() {
     cvlr_assume!(from.key == to.key);
     cvlr_assume!(from_wallet_amount_pre == to_wallet_amount_pre);
 
-    process_transfer_token(&account_infos, &token_instruction_data).unwrap();
+    process_transfer_token::<N_SIGNERS>(&account_infos, &token_instruction_data).unwrap();
 
     let from_wallet_amount_post = spl_token_account_get_amount(from);
     let to_wallet_amount_post = spl_token_account_get_amount(to);
@@ -44,7 +63,26 @@ pub fn rule_transfer_token_transfers_same_wallet() {
 }
 
 #[rule]
-pub fn rule_transfer_token_transfers_different_wallets() {
+pub fn rule_transfer_token_transfers_different_wallets_0() {
+    rule_transfer_token_transfers_different_wallets::<0>();
+}
+
+#[rule]
+pub fn rule_transfer_token_transfers_different_wallets_1() {
+    rule_transfer_token_transfers_different_wallets::<1>();
+}
+
+#[rule]
+pub fn rule_transfer_token_transfers_different_wallets_2() {
+    rule_transfer_token_transfers_different_wallets::<2>();
+}
+
+#[rule]
+pub fn rule_transfer_token_transfers_different_wallets_3() {
+    rule_transfer_token_transfers_different_wallets::<3>();
+}
+
+pub fn rule_transfer_token_transfers_different_wallets<const N_SIGNERS: usize>() {
     let account_infos = cvlr_deserialize_nondet_accounts();
     let account_info_iter = &mut account_infos.iter();
     let token_program: &AccountInfo = next_account_info(account_info_iter).unwrap();
@@ -61,7 +99,7 @@ pub fn rule_transfer_token_transfers_different_wallets() {
     let from_wallet_amount_pre = spl_token_account_get_amount(from);
     let to_wallet_amount_pre = spl_token_account_get_amount(to);
 
-    process_transfer_token(&account_infos, &token_instruction_data).unwrap();
+    process_transfer_token::<N_SIGNERS>(&account_infos, &token_instruction_data).unwrap();
 
     let from_wallet_amount_post = spl_token_account_get_amount(from);
     let to_wallet_amount_post = spl_token_account_get_amount(to);
@@ -91,7 +129,7 @@ pub fn rule_transfer_token_transfers_same_wallet_failing() {
     cvlr_assume!(from.key == to.key);
     cvlr_assume!(from_wallet_amount_pre == to_wallet_amount_pre);
 
-    process_transfer_token(&account_infos, &token_instruction_data).unwrap();
+    process_transfer_token::<0>(&account_infos, &token_instruction_data).unwrap();
 
     let from_wallet_amount_post = spl_token_account_get_amount(from);
     let to_wallet_amount_post = spl_token_account_get_amount(to);
@@ -103,7 +141,26 @@ pub fn rule_transfer_token_transfers_same_wallet_failing() {
 }
 
 #[rule]
-pub fn rule_mint_token_mints() {
+pub fn rule_mint_token_mints_0() {
+    rule_mint_token_mints::<0>();
+}
+
+#[rule]
+pub fn rule_mint_token_mints_1() {
+    rule_mint_token_mints::<1>();
+}
+
+#[rule]
+pub fn rule_mint_token_mints_2() {
+    rule_mint_token_mints::<2>();
+}
+
+#[rule]
+pub fn rule_mint_token_mints_3() {
+    rule_mint_token_mints::<3>();
+}
+
+pub fn rule_mint_token_mints<const N_SIGNERS: usize>() {
     let account_infos = cvlr_deserialize_nondet_accounts();
     let account_info_iter = &mut account_infos.iter();
     let token_program: &AccountInfo = next_account_info(account_info_iter).unwrap();
@@ -112,15 +169,13 @@ pub fn rule_mint_token_mints() {
     let _mint_authority: &AccountInfo = next_account_info(account_info_iter).unwrap();
 
     let amount: u64 = nondet();
-    let decimals: u8 = nondet();
     let mut token_instruction_data = Vec::new();
     token_instruction_data.extend_from_slice(&amount.to_le_bytes());
-    token_instruction_data.extend_from_slice(&decimals.to_le_bytes());
 
     let destination_wallet_amount_pre = spl_token_account_get_amount(destination);
     let mint_supply_pre = spl_mint_get_supply(mint);
 
-    process_mint_token(&account_infos, &token_instruction_data).unwrap();
+    process_mint_token::<N_SIGNERS>(&account_infos, &token_instruction_data).unwrap();
 
     let destination_wallet_amount_post = spl_token_account_get_amount(destination);
     let mint_supply_post = spl_mint_get_supply(mint);
@@ -131,7 +186,26 @@ pub fn rule_mint_token_mints() {
 }
 
 #[rule]
-pub fn rule_burn_token_burns() {
+pub fn rule_burn_token_burns_0() {
+    rule_burn_token_burns::<0>();
+}
+
+#[rule]
+pub fn rule_burn_token_burns_1() {
+    rule_burn_token_burns::<1>();
+}
+
+#[rule]
+pub fn rule_burn_token_burns_2() {
+    rule_burn_token_burns::<2>();
+}
+
+#[rule]
+pub fn rule_burn_token_burns_3() {
+    rule_burn_token_burns::<3>();
+}
+
+pub fn rule_burn_token_burns<const N_SIGNERS: usize>() {
     let account_infos = cvlr_deserialize_nondet_accounts();
     let account_info_iter = &mut account_infos.iter();
     let token_program: &AccountInfo = next_account_info(account_info_iter).unwrap();
@@ -146,7 +220,7 @@ pub fn rule_burn_token_burns() {
     let source_wallet_amount_pre = spl_token_account_get_amount(source);
     let mint_supply_pre = spl_mint_get_supply(mint);
 
-    process_burn_token(&account_infos, &token_instruction_data).unwrap();
+    process_burn_token::<N_SIGNERS>(&account_infos, &token_instruction_data).unwrap();
 
     let source_wallet_amount_post = spl_token_account_get_amount(source);
     let mint_supply_post = spl_mint_get_supply(mint);
@@ -157,7 +231,26 @@ pub fn rule_burn_token_burns() {
 }
 
 #[rule]
-pub fn rule_close_account_balance_is_zero_after_success() {
+pub fn rule_close_account_balance_is_zero_after_success_0() {
+    rule_close_account_balance_is_zero_after_success::<0>();
+}
+
+#[rule]
+pub fn rule_close_account_balance_is_zero_after_success_1() {
+    rule_close_account_balance_is_zero_after_success::<1>();
+}
+
+#[rule]
+pub fn rule_close_account_balance_is_zero_after_success_2() {
+    rule_close_account_balance_is_zero_after_success::<2>();
+}
+
+#[rule]
+pub fn rule_close_account_balance_is_zero_after_success_3() {
+    rule_close_account_balance_is_zero_after_success::<3>();
+}
+
+pub fn rule_close_account_balance_is_zero_after_success<const N_SIGNERS: usize>() {
     let account_infos = cvlr_deserialize_nondet_accounts();
     let account_info_iter = &mut account_infos.iter();
     let token_program: &AccountInfo = next_account_info(account_info_iter).unwrap();
@@ -167,7 +260,7 @@ pub fn rule_close_account_balance_is_zero_after_success() {
 
     let token_instruction_data = Vec::new();
 
-    process_close_account(&account_infos, &token_instruction_data).unwrap();
+    process_close_account::<N_SIGNERS>(&account_infos, &token_instruction_data).unwrap();
 
     let account_wallet_amount_post = spl_token_account_get_amount(account);
 
