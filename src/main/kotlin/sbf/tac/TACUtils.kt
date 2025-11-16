fff54839ccc8a4bac0b17315d93ff75713a0151d
Jorge Navas <navasjorgea@gmail.com>
2025-11-16 12:31:06 +0200

    fix(solana): CVT_nativeint_u64_from_u128/u256 (#8113)
    f447ad58c62e3147bf71288e8097a21ce85b75a1

--- src/main/kotlin/sbf/tac/TACUtils.kt
+++ src/main/kotlin/sbf/tac/TACUtils.kt
@@ -152,17 +152,9 @@ fun <TNum : INumValue<TNum>, TOffset : IOffset<TOffset>, TFlags: IPTANodeFlags<T
     return assign(res, TACExpr.Vec.Add(listOf(TACExpr.BinOp.ShiftLeft(high, c64E), exprBuilder.mask64(low))))
 }
 
-/** res = high << 64 + low **/
-context(SbfCFGToTAC<TNum, TOffset, TFlags>)
-fun <TNum : INumValue<TNum>, TOffset : IOffset<TOffset>, TFlags: IPTANodeFlags<TFlags>> mergeU128Raw(
-    res: TACSymbol.Var, low: TACExpr.Sym, high: TACExpr.Sym): TACCmd.Simple.AssigningCmd {
-    val c64E = exprBuilder.SIXTY_FOUR.asSym()
-    return assign(res, TACExpr.Vec.Add(listOf(TACExpr.BinOp.ShiftLeft(high, c64E), low)))
-}
-
 /** res = (w4 << 192) + (w3 << 128) + (w2 << 64) + w1 */
 context(SbfCFGToTAC<TNum, TOffset, TFlags>)
-fun <TNum : INumValue<TNum>, TOffset : IOffset<TOffset>, TFlags: IPTANodeFlags<TFlags>> mergeU256Raw(
+fun <TNum : INumValue<TNum>, TOffset : IOffset<TOffset>, TFlags: IPTANodeFlags<TFlags>> mergeU256(
     res: TACSymbol.Var, w1: TACExpr.Sym, w2: TACExpr.Sym, w3:TACExpr.Sym, w4: TACExpr.Sym): TACCmd.Simple.AssigningCmd {
     check(res.tag is Tag.Bit256) {"mergeU256 expects $res to be Tag.Bit256"}
 
@@ -172,9 +164,9 @@ fun <TNum : INumValue<TNum>, TOffset : IOffset<TOffset>, TFlags: IPTANodeFlags<T
     return assign(res, TACExpr.Vec.Add(
        listOf(
             TACExpr.BinOp.ShiftLeft(w4, c196),
-            TACExpr.BinOp.ShiftLeft(w3, c128),
-            TACExpr.BinOp.ShiftLeft(w2, c64),
-            w1
+            TACExpr.BinOp.ShiftLeft(exprBuilder.mask64(w3), c128),
+            TACExpr.BinOp.ShiftLeft(exprBuilder.mask64(w2), c64),
+            exprBuilder.mask64(w1)
         )
     ))
 }
